5e87652e314f916cd9117a77be158a3145fd6a6a
Simon Eisenmann <simon@struktur.de>
2016-05-30 15:19:22 +0200

    Fix docstring lint.

--- lockmap/lockmap.go
+++ lockmap/lockmap.go
@@ -4,27 +4,28 @@ import (
 	"sync"
 )
 
-// A threadsafe map of type string:interface{}.
+// LockMap is a threadsafe map of type string:interface{}.
 type LockMap struct {
 	items map[string]interface{}
 	sync.RWMutex
 }
 
+// New creates a new LockMap.
 func New() *LockMap {
 	return &LockMap{
 		items: make(map[string]interface{}),
 	}
 }
 
-// Sets the given value under the specified key.
+// Set sets the given value under the specified key.
 func (lm *LockMap) Set(key string, value interface{}) {
 	lm.Lock()
 	lm.items[key] = value
 	lm.Unlock()
 }
 
-// Sets the given value under the specified key if key is not already set.
-// Returns true when value was set, false otherwise.
+// SetIfAbsent sets the given value under the specified key if key is
+// not already set. Returns true when value was set, false otherwise.
 func (lm *LockMap) SetIfAbsent(key string, value interface{}) bool {
 	lm.Lock()
 	_, ok := lm.items[key]
@@ -36,7 +37,7 @@ func (lm *LockMap) SetIfAbsent(key string, value interface{}) bool {
 	return !ok
 }
 
-// Retrieves the element from the map by the given key.
+// Get retrieves the element from the map by the given key.
 func (lm *LockMap) Get(key string, value interface{}) (interface{}, bool) {
 	lm.RLock()
 	val, ok := lm.items[key]
@@ -45,7 +46,7 @@ func (lm *LockMap) Get(key string, value interface{}) (interface{}, bool) {
 	return val, ok
 }
 
-// Checks if the map has the given key.
+// Has checks if the map has the given key.
 func (lm *LockMap) Has(key string) bool {
 	lm.RLock()
 	_, ok := lm.items[key]
@@ -54,7 +55,7 @@ func (lm *LockMap) Has(key string) bool {
 	return ok
 }
 
-// Removes an element from the map.
+// Remove deletes an element from the map.
 func (lm *LockMap) Remove(key string) {
 	lm.Lock()
 	delete(lm.items, key)
