4a338d41da8d547fe6ce8048cff36a3e7e77caac
Simon Eisenmann <simon@struktur.de>
2016-06-24 13:25:09 +0200

    Fake enabled started flag when with master
    The Angular auth plugin provider, starts the refresher if it finds one.
    To avoid non-functional startups when not the master (eg. inside an
    iframe), we enable the started flag when no frame is created.

--- www/static/scripts/spreedbox-auth.js
+++ www/static/scripts/spreedbox-auth.js
@@ -693,6 +693,7 @@
 
 				if (!create) {
 					window.top.spreedboxAuthRefresher.link(refresher);
+					refresher.started = true // Fake start.
 					window.setTimeout(function() {
 						trigger(refresher, 'auth', getCurrentAuth(), null);
 					}, 0);
